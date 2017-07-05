23f6661549164fbf3a0c2504845fba88596175f9
tuhuuxxx <tuhuuxxx@gmail.com>
2017-07-05 13:31:04 +0700

    Add currency filter

---
+++ src/filters/currency.js
@@ -0,0 +1,5 @@
+export default number => {
+  return number.toFixed(0).replace(/./g, function (c, i, a) {
+    return i && c !== '.' && ((a.length - i) % 3 === 0) ? ',' + c : c
+  })
+}
---
+++ src/filters/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import currency from './currency'
+
+Vue.filter('currency', currency)
